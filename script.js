cffefda16b118ac23c51a8bbeacc7d3df4a060c7
devyndamato <devyn.damato@du.edu>
2026-02-18 16:10:38 -0700

    Add event listener to toggle preview section visibility

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const sneakPeekBtn = document.querySelector("#sneak-peek-btn");
+const previewSection = document.querySelector("#experience-preview");
 
+sneakPeekBtn.addEventListener("click", function () {
+  previewSection.classList.toggle("hidden");
+});
